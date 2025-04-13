697e6d441af090b494a2caca65ccf083f4e43d71
Fankey <arfan.izanagi456@gmail.com>
2025-04-13 15:20:08 +0800

    updated

--- Lab/04/src/Q1/TestLinkedList.java
+++ Lab/04/src/Q1/TestLinkedList.java
@@ -1,36 +1,51 @@
 package Q1;
 
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
 public class TestLinkedList {
     public static void main(String[] args) {
         MyLinkedList<Character> list = new MyLinkedList<>();
+
         list.addLast('a');
         list.addLast('b');
         list.addLast('c');
         list.addLast('d');
         list.addLast('e');
         
+        System.out.print("normal list: ");
+        list.print();
+        System.out.print("reverse list: ");
+        list.reverse();
         list.print();
 
-        list.reverse();
+        System.out.println("list size: " + list.size);
 
-        System.out.println(list.size);
+        System.out.println("first element: " + list.getFirst());
+        System.out.println("last element: " + list.getLast());
 
-        System.out.println(list.getFirst());
-        System.out.println(list.getLast());
+        System.out.println("remove 3rd element from list: " + list.remove(2));
+        System.out.print("current list: ");
+        list.print();
 
-        list.remove(2);
+        System.out.println("index of 'b' : " + list.indexOf('b'));
+        System.out.println("index of 'c' : " + list.indexOf('c'));
 
-        System.out.println(list.indexOf('b') + " " + list.indexOf('c'));
-
-        System.out.println(list.contains('c'));
-
+        System.out.println("contain 'c' : "+ list.contains('c'));
+        System.out.print("current list : ");
         list.print();
 
         list.set(0, 'j');
         list.set(1, 'a');
         list.set(2, 'v');
 
+        System.out.println("set 0th element to 'j' and 1st element to 'a' and 2nd element to 'v' ");
+        System.out.print("current list : ");
         list.print();
+
+        System.out.println("middle value of list: " + list.getMiddleValue());
+
+
+
         
     }
 }
